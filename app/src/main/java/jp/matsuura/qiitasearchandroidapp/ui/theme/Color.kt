6b3cd7efb796fdba2e5c0a0ffd73d5289a022859
matsuurayuki1219 <yuki.matsuura@progrit.co.jp>
2023-09-28 19:18:00 +0900

    feat: implement theme.

--- app/src/main/java/jp/matsuura/qiitasearchandroidapp/ui/theme/Color.kt
+++ app/src/main/java/jp/matsuura/qiitasearchandroidapp/ui/theme/Color.kt
@@ -9,3 +9,16 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Green40 = Color(0xFF55C500)
+
+val Black110 = Color(0xFF1E2020)
+
+val Gray10 = Color(0xFFF5F6F6)
+val Gray40 = Color(0xFFEDEEEE)
+val Gray60 = Color(0xFF2F3232)
+val Gray90 = Color(0xFF5F5F5F)
+
+val White = Color(0xFFFFFFFF)
+
+val Yellow90 = Color(0xFFF7A535)
--- app/src/main/java/jp/matsuura/qiitasearchandroidapp/ui/theme/Theme.kt
+++ app/src/main/java/jp/matsuura/qiitasearchandroidapp/ui/theme/Theme.kt
@@ -10,38 +10,35 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Green40,
+    secondary = Yellow90,
+    tertiary = Pink80,
+    secondaryContainer = Gray90,
+    onPrimaryContainer = Gray60,
+    background = Black110,
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = Green40,
+    secondary = Yellow90,
+    tertiary = Pink40,
+    secondaryContainer = Gray40,
+    onPrimaryContainer = White,
+    background = Gray10,
 )
 
 @Composable
 fun QiitaSearchAndroidAppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
@@ -49,7 +46,6 @@ fun QiitaSearchAndroidAppTheme(
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
-
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
